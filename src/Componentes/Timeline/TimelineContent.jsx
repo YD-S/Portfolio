d653523473eb82bd46ca136d32ee760648da3921
Yash <seehra.a@yahoo.com>
2025-05-29 18:54:36 +0200

    Minor update

--- src/Componentes/Timeline/TimelineContent.jsx
+++ src/Componentes/Timeline/TimelineContent.jsx
@@ -2,7 +2,7 @@
 export default function TimelineContent({children, year, id}) {
     let classes = "pl-24 relative before:w-4 before:h-4 before:bg-gradient-to-r before:from-purple-500 before:to-blue-500 before:absolute before:rounded-full before:left-[58px]"
     if (id !== 1)
-        classes += " mt-24"
+        classes += " mt-20"
     return (
         <div
             className={classes}>
